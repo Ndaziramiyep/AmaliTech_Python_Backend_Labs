bcac5e17dc0a6b78c4c1a2a03604e90fe5abf27a
Ndaziramiyep <patrickofficial30@gmail.com>
2026-07-22 12:56:11 +0200

    fix: resolve pre-commit pytest hook path and add pytest config to pyproject.tom

--- CLEAN_CODE_TESTING_GIT/secure-service-module/run_tests.bat
+++ CLEAN_CODE_TESTING_GIT/secure-service-module/run_tests.bat
@@ -1,3 +1,3 @@
 @echo off
-call venv\Scripts\activate.bat
+cd /d "C:\Users\patri\OneDrive\Desktop\AmaliTech\AmaliTech_Python_Backend_Labs\CLEAN_CODE_TESTING_GIT\secure-service-module"
 python -m pytest tests/
