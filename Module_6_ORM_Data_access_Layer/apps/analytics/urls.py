3570c0c15e691407c9bfed113eb0ab9c2a92b881
Ndaziramiyep <patrickofficial30@gmail.com>
2026-08-28 06:11:20 +0200

    restructuring the project

--- Module_6_ORM_Data_access_Layer/apps/analytics/urls.py
+++
@@ -1,9 +0,0 @@
-"""URL routes for the analytics app."""
-
-from django.urls import path
-
-from .views import URLAnalyticsView
-
-urlpatterns = [
-    path("<str:short_code>/", URLAnalyticsView.as_view(), name="url-analytics"),
-]
