23e8851863b104b6deb671569569dc7c35b965a2
Ndaziramiyep <patrickofficial30@gmail.com>
2026-09-09 16:30:51 +0200

    Scaffold url-service Django project

---
+++ Module_9_URL_Shortener/Enterprise-Grade_URL_Shortener/services/url-service/Config/wsgi.py
@@ -0,0 +1,11 @@
+"""
+WSGI config for the url-service project.
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Config.settings")
+
+application = get_wsgi_application()
